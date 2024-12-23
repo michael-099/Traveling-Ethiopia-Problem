808503bdf359d739261a54de45abe52be4741c0c
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-12-23 09:38:14 +0300

    -m

---
+++ tavel all cties.py
@@ -0,0 +1,13 @@
+def traverse_all_cities(cities, roads, start_city, strategy):
+    """
+    Parameters:
+    - cities: List of city names.
+    - roads: Dictionary with city connections as {city: [(connected_city, distance)]}.
+    - start_city: The city to start the journey.
+    - strategy: The uninformed search strategy to use ('bfs' or 'dfs').
+    
+    Returns:
+    - path: List of cities representing the traversal path.
+    - cost: Total cost (distance) of the traversal.
+    """
+    pass
--- uninformed_path_finder.py
+++ uninformed_path_finder.py
@@ -1,6 +1,7 @@
 # Implement Depth-First Search (DFS) to find a feasible path to the target city.
 # Implement Breadth-First Search (BFS) to find the shortest path based on the number of roads (unweighted).
-def uninformed_path_finder_BFS(cities, roads, start_city, goal_city, strategy):
+from collections import deque
+def uninformed_path_finder(cities, roads, start_city, goal_city, strategy):
     """
     Parameters:
     - cities: List of city names.
@@ -14,14 +15,51 @@ def uninformed_path_finder_BFS(cities, roads, start_city, goal_city, strategy):
     - cost: Total cost (number of steps or distance) of the path.
     """
     
-    def BFS():
-        pass
+    def BFS(start_city,roads,goal_city):
+        queue=deque()
+        queue.append(start_city)
+        visited=[start_city]
+        arr=[]
+        while queue:
+            curr=queue.popleft()
+            print(curr)
+            arr.append(curr)
+            for i in range(len(roads[curr])):
+                if roads[curr][i]==goal_city:
+                    visited.append(roads[curr][i])
+                    print(visited)
+                    return
+                if roads[curr][i]not in visited:
+                    visited.append(roads[curr][i])
+                    queue.append(roads[curr][i])        
+        print(visited)
+        
     
-    def DFS():
-        pass
+    def DFS(start_city,goal_city,roads):
+        stack=[]
+        
+            
+            
+        
+        
+        
+    
     
     if strategy=="BFS":
-        return BFS()
+       BFS(start_city,roads,goal_city)
     else :
-        return DFS()
-    
+        DFS()
+
+
+if __name__ == "__main__":
+    cities = ['Addis Ababa', 'Bahir Dar', 'Gondar', 'Hawassa', 'Mekelle']
+    roads = {
+    'Addis Ababa': ['Bahir Dar', 'Hawassa'],
+    'Bahir Dar': ['Addis Ababa', 'Gondar'],
+    'Gondar': ['Bahir Dar', 'Mekelle'],
+    'Hawassa': ['Addis Ababa'],
+    'Mekelle': ['Gondar']
+    }
+    print("hello")
+
+    uninformed_path_finder(cities,roads,"Bahir Dar","Gondar","BFS")
